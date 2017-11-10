a12c6bfe2f5835d5cc17a4f933a5c94cf6bf223a
sunyeming <206415831@qq.com>
2017-11-10 21:58:16 +0800

    类型改变

--- message/messag.go
+++ message/messag.go
@@ -50,7 +50,7 @@ func NewMessage() *Message {
 }
 
 // 打包原生字符串
-func (this *Message) Package(buf []byte) error {
+func (m *Message) Package(buf []byte) error {
 	l := len(buf)
 	if l == 0 {
 		return nil
@@ -68,15 +68,15 @@ func (this *Message) Package(buf []byte) error {
 
 	// 压缩后长度比原来小， 就保存压缩数据
 	if c && b.Len() < l {
-		this.SetUnlen(uint16(l))
-		this.SetLen(uint16(b.Len()))
-		this.Data = make([]byte, b.Len())
-		copy(this.Data[:], b.Bytes())
+		m.SetUnlen(uint16(l))
+		m.SetLen(uint16(b.Len()))
+		m.Data = make([]byte, b.Len())
+		copy(m.Data[:], b.Bytes())
 	} else {
-		this.SetUnlen(uint16(l))
-		this.SetLen(uint16(l))
-		this.Data = make([]byte, l)
-		copy(this.Data[:], buf)
+		m.SetUnlen(uint16(l))
+		m.SetLen(uint16(l))
+		m.Data = make([]byte, l)
+		copy(m.Data[:], buf)
 	}
 
 	return nil
@@ -84,26 +84,26 @@ func (this *Message) Package(buf []byte) error {
 }
 
 // 解包原生字符串
-func (this *Message) Unpackage() ([]byte, error) {
-	if this.GetLen() == 0 {
+func (m *Message) Unpackage() ([]byte, error) {
+	if m.GetLen() == 0 {
 		return []byte(""), nil
 	}
-	if this.GetLen() == this.GetUnlen() {
-		data := make([]byte, this.GetLen())
-		copy(data[:], this.Data)
+	if m.GetLen() == m.GetUnlen() {
+		data := make([]byte, m.GetLen())
+		copy(data[:], m.Data)
 		return data, nil
-	} else if this.GetLen() < this.GetUnlen() {
+	} else if m.GetLen() < m.GetUnlen() {
 		var b bytes.Buffer
-		b.Write(this.Data)
+		b.Write(m.Data)
 		r, err := zlib.NewReader(&b)
 		if err != nil {
 			return []byte(""), err
 		}
 		defer r.Close()
 
-		data := make([]byte, this.GetUnlen())
+		data := make([]byte, m.GetUnlen())
 		l, _ := r.Read(data)
-		if l != int(this.GetUnlen()) {
+		if l != int(m.GetUnlen()) {
 			return []byte(""), errors.New("uncompress erro")
 		}
 		return data, nil
@@ -113,17 +113,17 @@ func (this *Message) Unpackage() ([]byte, error) {
 }
 
 // 打包protubuf消息
-func (this *Message) PackagePbmsg(pb proto.Message) error {
+func (m *Message) PackagePbmsg(pb proto.Message) error {
 	buf, err := proto.Marshal(pb)
 	if err != nil {
 		return err
 	}
-	return this.Package(buf)
+	return m.Package(buf)
 }
 
 // 解包protobuf消息
-func (this *Message) UnpackagePbmsg(pb proto.Message) error {
-	data, err := this.Unpackage()
+func (m *Message) UnpackagePbmsg(pb proto.Message) error {
+	data, err := m.Unpackage()
 	if err != nil {
 		return err
 	}
@@ -131,17 +131,17 @@ func (this *Message) UnpackagePbmsg(pb proto.Message) error {
 }
 
 // 根据消息长度初始化消息体内存
-func (this *Message) InitData() {
-	if this.GetLen() == 0 {
+func (m *Message) InitData() {
+	if m.GetLen() == 0 {
 		return
 	}
-	this.Data = make([]byte, this.GetLen())
+	m.Data = make([]byte, m.GetLen())
 }
 
 // 获取t位置开始的两个字节的数值
-func (this *Message) get16(t uint32) uint16 {
+func (m *Message) get16(t uint32) uint16 {
 	buf := bytes.NewBuffer(make([]byte, 0, MessageHeaderLen))
-	buf.Write(this.Header[t : t+2])
+	buf.Write(m.Header[t : t+2])
 
 	i16 := make([]byte, 2)
 	buf.Read(i16)
@@ -149,14 +149,14 @@ func (this *Message) get16(t uint32) uint16 {
 }
 
 // 将id设置到t位置开始的两个字节内
-func (this *Message) set16(t uint32, id uint16) {
-	binary.BigEndian.PutUint16(this.Header[t:t+2], id)
+func (m *Message) set16(t uint32, id uint16) {
+	binary.BigEndian.PutUint16(m.Header[t:t+2], id)
 }
 
 // 获取t位置开始的四个字节的数值
-func (this *Message) get32(t uint32) uint32 {
+func (m *Message) get32(t uint32) uint32 {
 	buf := bytes.NewBuffer(make([]byte, 0, MessageHeaderLen))
-	buf.Write(this.Header[t : t+4])
+	buf.Write(m.Header[t : t+4])
 
 	i32 := make([]byte, 4)
 	buf.Read(i32)
@@ -164,72 +164,72 @@ func (this *Message) get32(t uint32) uint32 {
 }
 
 // 将id设置到t位置开始的四个字节内
-func (this *Message) set32(t uint32, id uint32) {
-	binary.BigEndian.PutUint32(this.Header[t:t+4], id)
+func (m *Message) set32(t uint32, id uint32) {
+	binary.BigEndian.PutUint32(m.Header[t:t+4], id)
 }
 
 // 获取消息id(消息头前四位0 - 3)
-func (this *Message) GetId() uint32 {
-	return this.get32(MessageIdBit)
+func (m *Message) GetId() uint32 {
+	return m.get32(MessageIdBit)
 }
 
 // 设置消息id
-func (this *Message) SetId(id uint32) {
-	this.set32(MessageIdBit, id)
+func (m *Message) SetId(id uint32) {
+	m.set32(MessageIdBit, id)
 }
 
 // 获取命令（消息头8 - 9）
-func (this *Message) GetCmd() uint16 {
-	return this.get16(MessageCmdBit)
+func (m *Message) GetCmd() uint16 {
+	return m.get16(MessageCmdBit)
 }
-func (this *Message) SetCmd(cmd uint16) {
-	this.set16(MessageCmdBit, cmd)
+func (m *Message) SetCmd(cmd uint16) {
+	m.set16(MessageCmdBit, cmd)
 }
 
 // 获取消息序号（消息头10 - 11）
-func (this *Message) GetSeq() uint16 {
-	return this.get16(MessageSeqBit)
+func (m *Message) GetSeq() uint16 {
+	return m.get16(MessageSeqBit)
 }
 
-func (this *Message) SetSeq(seq uint16) {
-	this.set16(MessageSeqBit, seq)
+func (m *Message) SetSeq(seq uint16) {
+	m.set16(MessageSeqBit, seq)
 }
 
 // 获取消息未压缩长度（消息头4 - 5）
-func (this *Message) GetUnlen() uint16 {
-	return this.get16(MessageUnlenBit)
+func (m *Message) GetUnlen() uint16 {
+	return m.get16(MessageUnlenBit)
 }
 
-func (this *Message) SetUnlen(len uint16) {
-	this.set16(MessageUnlenBit, len)
+func (m *Message) SetUnlen(len uint16) {
+	m.set16(MessageUnlenBit, len)
 }
 
 // 获取消息体长度（消息头 6 - 7）
-func (this *Message) GetLen() uint16 {
-	return this.get16(MessageLenBit)
+func (m *Message) GetLen() uint16 {
+	return m.get16(MessageLenBit)
 }
 
-func (this *Message) SetLen(len uint16) {
-	this.set16(MessageLenBit, len)
+func (m *Message) SetLen(len uint16) {
+	m.set16(MessageLenBit, len)
 }
 
 // 获取消息返回值（消息头 12 - 13）
-func (this *Message) GetRet() uint16 {
-	return this.get16(MessageRetBit)
+func (m *Message) GetRet() uint16 {
+	return m.get16(MessageRetBit)
 }
 
-func (this *Message) SetRet(ret uint16) {
-	this.set16(MessageRetBit, ret)
+func (m *Message) SetRet(ret uint16) {
+	m.set16(MessageRetBit, ret)
 }
 
 // 获取消息标志（消息头 14 - 16）
-func (this *Message) GetMask(mask uint16) bool {
-	i := this.get16(MessageMaskBit)
+func (m *Message) GetMask(mask uint16) bool {
+	i := m.get16(MessageMaskBit)
 	return (i & (1 << mask)) != 0
 }
 
-func (this *Message) SetMask(mask uint16, b bool) {
-	i := this.get16(MessageMaskBit)
+func (m *Message) SetMask(mask uint16, b bool) {
+	i := m.get16(MessageMaskBit)
 
 	if b {
 		i |= 1 << mask
@@ -237,19 +237,19 @@ func (this *Message) SetMask(mask uint16, b bool) {
 		i &= ^(1 << mask)
 	}
 
-	this.set16(MessageMaskBit, i)
+	m.set16(MessageMaskBit, i)
 }
 
 // 打印的借口实现
-func (this *Message) String() string {
-	return fmt.Sprintf("id: %d, unlen: %d, len: %d, cmd: %d, seq: %d, ret: %d", this.GetId(), this.GetUnlen(), this.GetLen(), this.GetCmd(), this.GetSeq(), this.GetRet())
+func (m *Message) String() string {
+	return fmt.Sprintf("id: %d, unlen: %d, len: %d, cmd: %d, seq: %d, ret: %d", m.GetId(), m.GetUnlen(), m.GetLen(), m.GetCmd(), m.GetSeq(), m.GetRet())
 }
 
 // 消息的拷贝
-func (this *Message) Copy() *Message {
+func (m *Message) Copy() *Message {
 	newMsg := NewMessage()
-	newMsg.Data = make([]byte, this.GetLen())
-	copy(newMsg.Header[:], this.Header)
-	copy(newMsg.Data[:], this.Data)
+	newMsg.Data = make([]byte, m.GetLen())
+	copy(newMsg.Header[:], m.Header)
+	copy(newMsg.Data[:], m.Data)
 	return newMsg
 }
